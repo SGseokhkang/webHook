8fc5c5663c2a025009ceb98521fc14e8fa3e6880
“seokhkang” <seokhkang@smilegate.com>
2023-08-31 19:10:36 +0900

    8/31

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ async function checkFigmaComments(lastCommentId = null) {
     const latestComment = comments[0];
     if (latestComment.id !== lastCommentId) {
       console.log("새로운 코멘트 감지:", latestComment.message);
-      console.log("새로운 코멘트유저 감지:", latestComment.user);
+      console.log("새로운 코멘트유저 감지:", latestComment.user.handle);
 
       const url = "https://schat.smilegate.net/hooks/64e477d6892ec40472d71732/rjDH9MFQpPzFsjvQazM5764Co8CW2iQzZfFi6TqpuWud6NAE";
 
@@ -33,7 +33,7 @@ async function checkFigmaComments(lastCommentId = null) {
           text: `${latestComment.message}`,
           attachments: [
               {
-                  title: `[${latestComment.user}]:${latestComment.message}`,
+                  title: `[${latestComment.user.handle}]:${latestComment.message}`,
                   title_link: "https://schat.smilegate.net",
                   text: "첨부 파일의 본문 영역입니다. 첨부파일도 추가하여 보낼 수 있습니다.",
                   image_url: "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png",

370046687d0424a0a304b6722693385a6ef5e2d0
“seokhkang” <seokhkang@smilegate.com>
2023-09-07 17:00:23 +0900

    9/7

--- app.js
+++ app.js
@@ -21,20 +21,43 @@ app.post('/webhook-library', (req, res) => {
 
 
 // 웹훅 댓글
-app.post('/webhook-ppool', (req, res) => {
+app.post('/webhook-ppool', async (req, res) => {
+    console.log('Received Figma event:', req.body);
+    const latestWebhookData = req.body;  // 웹훅 데이터 저장
 
-        console.log('Received Figma event:', req.body);
-        latestWebhookData = req.body;  // 웹훅 데이터 저장
+    let latestComment;
+    if (req.body && req.body.comment) {
+        // 코멘트를 하나의 문자열로 합칩니다.
+        latestComment = req.body.comment.map(item => item.text || `[Mention: ${item.mention}]`).join(' ');
+    } else {
+        latestComment = "No comment data found.";
+    }
 
-        if (req.body && req.body.comment) {
-            // 코멘트를 하나의 문자열로 합칩니다.
-            latestComment = req.body.comment.map(item => item.text || `[Mention: ${item.mention}]`).join(' ');
-        } else {
-            latestComment = "No comment data found.";
-        }
+    // Webhook URL로 데이터 전송
+    const webhookUrl = 'https://schat.smilegate.net/hooks/64f9818cac7ea68c73af63d4/bQZNbyma8LabhRGwnzYA3JJRrrJ5YLybkqLMydAHGx5Keynm';
+    const postData = {
+        text: "markdown을 활용하여 메시지 내용 입력하여 보낼 수 있습니다.",
+        attachments: [{
+            title: "첨부 파일의 타이틀 영역입니다",
+            title_link: "https://schat.smilegate.net",
+            text: latestComment, // Modify this to send the `latestComment` or any other data you wish.
+            image_url: "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png",
+            color: "#764FA5"
+        }]
+    };
 
-        res.status(200).send('OK');
-  
+    try {
+        const response = await axios.post(webhookUrl, postData, {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+        console.log('Sent data to webhook:', response.data);
+    } catch (error) {
+        console.error('Error sending data to webhook:', error);
+    }
+
+    res.status(200).send('OK');
 });
 
 // 최신 웹훅 데이터를 가져오는 엔드포인트

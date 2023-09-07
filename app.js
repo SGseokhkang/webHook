bf6923d0163c99f9cdd8fa7e5b34d14d004b51df
“seokhkang” <seokhkang@smilegate.com>
2023-09-07 17:42:28 +0900

    9/7

--- app.js
+++ app.js
@@ -11,9 +11,37 @@ let latestLibraryData = {};  // 웹훅 데이터를 저장하기 위한 변수
 let latestWebhookData = {};  // 웹훅 데이터를 저장하기 위한 변수
 
 // 웹훅 라이브러리
-app.post('/webhook-library', (req, res) => {
+app.post('/webhook-library', async (req, res) => {
     console.log('Figma 이벤트를 받았습니다:', req.body);
     latestLibraryData = req.body;  // 웹훅 데이터 저장
+    
+    const libraryComponents = req.body.created_components;
+    const libraryPostID = req.body.triggered_by.handle;
+
+     // Webhook URL로 데이터 전송
+     const webhookUrl = 'https://schat.smilegate.net/hooks/64e477d6892ec40472d71732/rjDH9MFQpPzFsjvQazM5764Co8CW2iQzZfFi6TqpuWud6NAE';
+     const postData = {
+         text: `게시자 ID: ${libraryPostID} 님이 ppool 라이브러리를 Publish 했습니다.`,
+         attachments: [{
+             title: "라이브러리",
+             title_link: "https://schat.smilegate.net",
+             text: libraryComponents,
+             color: "#111111"
+         }]
+     };
+
+     try {
+         const response = await axios.post(webhookUrl, postData, {
+             headers: {
+                 'Content-Type': 'application/json'
+             }
+         });
+         console.log('웹훅으로 데이터를 보냈습니다:', response.data);
+     } catch (error) {
+         console.error('웹훅으로 데이터를 보내는 중 오류가 발생했습니다:', error);
+     }
+
+
     res.status(200).send('OK');
 });
 

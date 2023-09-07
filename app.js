c895f3c7bdf7b4dec08f27a2f81ef2776ea6615c
“seokhkang” <seokhkang@smilegate.com>
2023-09-07 17:07:36 +0900

    9/7

--- app.js
+++ app.js
@@ -6,54 +6,51 @@ const port = 3000;
 
 app.use(express.json());
 
-let latestComment = "No comments yet.";
+let latestComment = "아직 댓글이 없습니다.";
+let latestLibraryData = {};  // 웹훅 데이터를 저장하기 위한 변수
 let latestWebhookData = {};  // 웹훅 데이터를 저장하기 위한 변수
 
 // 웹훅 라이브러리
 app.post('/webhook-library', (req, res) => {
-
-    console.log('Received Figma event:', req.body);
+    console.log('Figma 이벤트를 받았습니다:', req.body);
     latestLibraryData = req.body;  // 웹훅 데이터 저장
-
     res.status(200).send('OK');
-
 });
-
 
 // 웹훅 댓글
 app.post('/webhook-ppool', async (req, res) => {
-    console.log('Received Figma event:', req.body);
-    const latestComment = req.body;  // 웹훅 데이터 저장
+    console.log('Figma 이벤트를 받았습니다:', req.body);
+    latestWebhookData = req.body;  // 웹훅 데이터 저장
 
     if (req.body && req.body.comment) {
         // 코멘트를 하나의 문자열로 합칩니다.
         latestComment = req.body.comment.map(item => item.text || `[Mention: ${item.mention}]`).join(' ');
+
+        // Webhook URL로 데이터 전송
+        const webhookUrl = 'https://schat.smilegate.net/hooks/64f9818cac7ea68c73af63d4/bQZNbyma8LabhRGwnzYA3JJRrrJ5YLybkqLMydAHGx5Keynm';
+        const postData = {
+            text: "마크다운을 활용하여 메시지 내용을 입력하여 보낼 수 있습니다.",
+            attachments: [{
+                title: "첨부 파일의 타이틀 영역입니다",
+                title_link: "https://schat.smilegate.net",
+                text: latestComment, // 이 부분을 원하는 데이터로 수정하세요.
+                image_url: "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png",
+                color: "#764FA5"
+            }]
+        };
+
+        try {
+            const response = await axios.post(webhookUrl, postData, {
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            });
+            console.log('웹훅으로 데이터를 보냈습니다:', response.data);
+        } catch (error) {
+            console.error('웹훅으로 데이터를 보내는 중 오류가 발생했습니다:', error);
+        }
     } else {
-        latestComment = "No comment data found.";
-    }
-
-    // Webhook URL로 데이터 전송
-    const webhookUrl = 'https://schat.smilegate.net/hooks/64f9818cac7ea68c73af63d4/bQZNbyma8LabhRGwnzYA3JJRrrJ5YLybkqLMydAHGx5Keynm';
-    const postData = {
-        text: "markdown을 활용하여 메시지 내용 입력하여 보낼 수 있습니다.",
-        attachments: [{
-            title: "첨부 파일의 타이틀 영역입니다",
-            title_link: "https://schat.smilegate.net",
-            text: latestComment, // Modify this to send the `latestComment` or any other data you wish.
-            image_url: "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png",
-            color: "#764FA5"
-        }]
-    };
-
-    try {
-        const response = await axios.post(webhookUrl, postData, {
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        });
-        console.log('Sent data to webhook:', response.data);
-    } catch (error) {
-        console.error('Error sending data to webhook:', error);
+        latestComment = "댓글 데이터를 찾을 수 없습니다.";
     }
 
     res.status(200).send('OK');
@@ -74,5 +71,5 @@ app.get('/', (req, res) => {
 });
 
 app.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}`);
+    console.log(`서버가 http://localhost:${port}에서 실행 중입니다.`);
 });

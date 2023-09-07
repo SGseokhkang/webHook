c7c8e6313ca30c820ccf62df36a843ceb86202c5
“seokhkang” <seokhkang@smilegate.com>
2023-09-07 16:00:15 +0900

    9/7

--- app.js
+++ app.js
@@ -3,41 +3,43 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const port = 3000;
-const config = require('./config');
-
-console.log(`Webhook ID: ${config.webhookId}`);
-console.log(`Figma Token: ${config.figmaToken}`);
 
 app.use(express.json());
 
 let latestComment = "No comments yet.";
+let latestWebhookData = {};  // 웹훅 데이터를 저장하기 위한 변수
 
-// 웹훅 로그 가져오기
-app.get('/webhook-ppool', async (req, res) => {
-  try {
-    const webhookId = "577584";  // 웹훅 아이디
-    const figmaToken = "figd_Do-_y5XpGO6REo9QYHI3rGJ-DFb6EnWkONYnc1HH";  // Figma 토큰
-    const response = await axios.get(`https://api.figma.com/v2/webhooks/:${webhookId}`, {
-      headers: {
-        'X-FIGMA-TOKEN': figmaToken,
-      },
-    });
+// 웹훅 엔드포인트 설정
+app.post('/', (req, res) => {
+    const passcode = req.headers['x-figma-passcode'];
 
-    const requests = response.data.requests;
-    const lastRequest = requests[requests.length - 1];  // 배열의 마지막 요소
+    if (passcode === 'ppool12345') {  // 설정한 passcode와 일치하는지 확인
+        console.log('Received Figma event:', req.body);
+        latestWebhookData = req.body;  // 웹훅 데이터 저장
 
-    res.json(lastRequest);  // 마지막 요소만 응답으로 보냅니다
-  } catch (error) {
-    res.status(500).json({ error: error.toString() });
-  }
+        if (req.body && req.body.comment) {
+            // 코멘트를 하나의 문자열로 합칩니다.
+            latestComment = req.body.comment.map(item => item.text || `[Mention: ${item.mention}]`).join(' ');
+        } else {
+            latestComment = "No comment data found.";
+        }
+
+        res.status(200).send('OK');
+    } else {
+        res.status(401).send('Unauthorized');
+    }
 });
 
+// 최신 웹훅 데이터를 가져오는 엔드포인트
+app.get('/latest-webhook-data', (req, res) => {
+    res.json(latestWebhookData);
+});
 
 // 웹 페이지 라우팅
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+    console.log(`Server is running at http://localhost:${port}`);
 });

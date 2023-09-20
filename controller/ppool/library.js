3ca3c1cfc68af48dee3766bc8cc5844a6773da35
“seokhkang” <seokhkang@smilegate.com>
2023-09-20 11:48:47 +0900

    9/20

--- controller/ppool/library.js
+++ controller/ppool/library.js
@@ -7,10 +7,10 @@ exports.webhookLibrary = async (req, res) => {
     latestLibraryData = req.body;
 
     const libraryPostID = req.body.triggered_by.handle;
-    const created_components = req.body.created_components;
-    const created_styles = req.body.created_styles;
-    const deleted_components = deleted_styles;
-    const modified_styles = req.body.created_components;
+    // const created_components = req.body.created_components;
+    // const created_styles = req.body.created_styles;
+    // const deleted_components = deleted_styles;
+    // const modified_styles = req.body.created_components;
 
 
     const webhookUrl = 'https://schat.smilegate.net/hooks/64e477d6892ec40472d71732/rjDH9MFQpPzFsjvQazM5764Co8CW2iQzZfFi6TqpuWud6NAE';
@@ -19,10 +19,23 @@ exports.webhookLibrary = async (req, res) => {
         attachments: [{
             title: "라이브러리",
             title_link: "https://schat.smilegate.net",
-            text: `새로 만든 컴포넌트: ${created_components}\n새로 만든 스타일: ${created_styles}\n삭제 된 컴포넌트: ${deleted_components}\n수정 된 스타일: ${modified_styles} `,
+            text: 'test',
             color: "#111111"
         }]
     };
+
+
+    // const webhookUrl = 'https://schat.smilegate.net/hooks/64e477d6892ec40472d71732/rjDH9MFQpPzFsjvQazM5764Co8CW2iQzZfFi6TqpuWud6NAE';
+    // const postData = {
+    //     text: `게시자 ID: ${libraryPostID} 님이 ppool 라이브러리를 Publish 했습니다.`,
+    //     attachments: [{
+    //         title: "라이브러리",
+    //         title_link: "https://schat.smilegate.net",
+    //         text: `새로 만든 컴포넌트: ${created_components}\n새로 만든 스타일: ${created_styles}\n삭제 된 컴포넌트: ${deleted_components}\n수정 된 스타일: ${modified_styles} `,
+    //         color: "#111111"
+    //     }]
+    // };
+
 
     try {
         const response = await axios.post(webhookUrl, postData, {

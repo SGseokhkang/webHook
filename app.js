7522b3c1b2a6a30cd8007a5a9765c35ed2499d8a
“seokhkang” <seokhkang@smilegate.com>
2023-10-18 14:33:46 +0900

    10/18

--- app.js
+++ app.js
@@ -20,28 +20,5 @@ app.use('/weekly-report', stoveWeeklyReport);
 
 app.listen(port, () => {
     console.log(`서버가 http://localhost:${port}에서 실행 중입니다.`);
-    
-    const sendAutomaticPost = () => {
-        fetch('http://localhost:' + port + '/weekly-report', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            // 예시 데이터입니다. 필요한 데이터로 변경하시면 됩니다.
-            body: JSON.stringify({
-                data: 'test data'
-            })
-        })
-        .then(response => response.json())
-        .then(data => {
-            console.log(data);
-        })
-        .catch(error => {
-            console.error('Error:', error);
-        });
-    };
-
-    cron.schedule('22 14 * * 3', sendAutomaticPost);
-
 });
 
--- controller/stove/weeklyWorkReport.js
+++ controller/stove/weeklyWorkReport.js
@@ -1,27 +1,34 @@
 const fetch = require('node-fetch');
+const cron = require('node-cron');
 
 exports.weeklyReport = async (req, res) => {
     const WEBHOOK_URL = 'https://schat.smilegate.net/hooks/652f5ed5aa06345cc50e5c86/Fiepqgxx9QFMSRpsu5LBxKMc2NbtEt4T5tPfy2KJ6NcNxbKo';
 
-    const payload = {
-        text: "주간업무 쓰세요!!!!!!!!!!까먹지말고!!!!!!!!"
+    const sendAutomaticPost = async () => {
+        const payload = {
+            text: "주간업무 쓰세요!!!!!!!!!!까먹지말고!!!!!!!!"
+        };
+
+        try {
+            const response = await fetch(WEBHOOK_URL, {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(payload)
+            });
+
+            if (!response.ok) {
+                throw new Error('Failed to send the message');
+            }
+
+            console.log('Message sent successfully!');
+        } catch (error) {
+            console.error('Error sending message:', error);
+        }
     };
 
-    try {
-        const response = await fetch(WEBHOOK_URL, {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify(payload)
-        });
-
-        if (!response.ok) {
-            throw new Error('Failed to send the message');
-        }
-
-        console.log('Message sent successfully!');
-        res.status(200).send({ message: "Message sent successfully!" });
-    } catch (error) {
-        console.error('Error sending message:', error);
-        res.status(500).send({ message: "Error sending message" });
-    }
+    // 스케줄러 초기화
+    cron.schedule('36 14 * * 3', sendAutomaticPost);
+    
+    // 요청에 대한 응답 (API 호출 시 바로 응답됨)
+    res.status(200).send({ message: "Scheduler initialized" });
 };

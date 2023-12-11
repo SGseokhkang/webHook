0019f94ea8ed63599fc61782fe990fa86c63d8c2
“seokhkang” <seokhkang@smilegate.com>
2023-12-11 11:08:17 +0900

    12/11

--- scheduler.js
+++ scheduler.js
@@ -1,19 +1,8 @@
 const fetch = require('node-fetch');
 const cron = require('node-cron');
 
-let count = 0; // 호출 횟수를 추적하는 변수
-
 const sendAutomaticPost = () => {
-    count++;
-    console.log('Function called', count, 'times');
-
-    // 함수를 5회 호출한 후 cron 작업 중지
-    if (count >= 5) {
-        scheduledTask.stop();
-        console.log('Cron job stopped after 5 calls');
-    }
-
-    fetch('https://port-0-webhook-6w1j2allyzyvdu.sel5.cloudtype.app/manitoBot', {
+    fetch('https://port-0-webhook-6w1j2allyzyvdu.sel5.cloudtype.app/weekly-report', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
@@ -31,8 +20,7 @@ const sendAutomaticPost = () => {
     });
 };
 
-// 10초 간격으로 sendAutomaticPost 함수 호출
-const scheduledTask = cron.schedule('*/10 * * * * *', sendAutomaticPost);
+cron.schedule('00 07,08 * * 4', sendAutomaticPost);
 
 
 

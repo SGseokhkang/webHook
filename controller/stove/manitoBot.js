85e7046e2d368071dae329c4c4dd2edad0e1becc
“seokhkang” <seokhkang@smilegate.com>
2023-12-11 10:40:23 +0900

    12/11

--- controller/stove/manitoBot.js
+++ controller/stove/manitoBot.js
@@ -1,11 +1,18 @@
 const fetch = require('node-fetch');
 
+// 메시지 목록 정의
+const messages = ["테스트 1", "테스트 2", "테스트 3", "테스트 4", "테스트 5"];
+let messageIndex = 0; // 현재 메시지 인덱스
+
 exports.manitoBot = async (req, res) => {
     const WEBHOOK_URL = 'https://schat.smilegate.net/hooks/6572bc5a8c41792fdaf04855/XXitQ8me2SazHeDz9EyzkwojtuFgE8QvPBNXnGPp4YSJpWzZ';
 
     const payload = {
-        text: "마니또 테스트"
+        text: messages[messageIndex]
     };
+
+    // 다음 메시지를 위한 인덱스 업데이트
+    messageIndex = (messageIndex + 1) % messages.length;
 
     try {
         const response = await fetch(WEBHOOK_URL, {

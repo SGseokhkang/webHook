7a74abc49a14e74cad8a17f5b9286870b101263b
“seokhkang” <seokhkang@smilegate.com>
2023-12-11 14:30:53 +0900

    12/11

--- manito.js
+++ manito.js
@@ -2,8 +2,19 @@ const fetch = require('node-fetch');
 const cron = require('node-cron');
 
 let count = 0; // 호출 횟수 추적
-let scheduledTask = cron.schedule('*/10 * * * * *', sendAutomaticPost); // cron 작업 초기 설정
 let maxAttempts = 6; // 최대 시도 횟수
+
+// 한국시간 기준 12월 11일 오후 2시 35분에 시작하는 cron 작업 설정
+let startTask = cron.schedule('35 14 11 12 *', () => {
+    sendAutomaticPost();
+    // 첫 실행 후 10초 간격으로 추가 실행을 위한 작업 설정
+    scheduledTask = cron.schedule('*/10 * * * * *', sendAutomaticPost);
+}, {
+    scheduled: false,
+    timezone: "Asia/Seoul"
+});
+
+let scheduledTask;
 
 function sendAutomaticPost() {
     count++;
@@ -12,8 +23,10 @@ function sendAutomaticPost() {
     // 최대 시도 횟수 도달 시 cron 작업 중지
     if (count >= maxAttempts) {
         console.log(`Stopping cron job after ${maxAttempts} attempts`);
-        scheduledTask.destroy();
-        scheduledTask = null;
+        if (scheduledTask) {
+            scheduledTask.destroy();
+            scheduledTask = null;
+        }
         return;
     }
 
@@ -38,12 +51,8 @@ function sendAutomaticPost() {
     })
     .catch(error => {
         console.error('Error:', error);
-        if (scheduledTask) {
-            count--; // 오류 발생 시 count 감소
-            console.log('Attempting to resend...');
-        }
     });
 }
 
-// 필요한 경우 cron 작업을 다시 시작할 수 있습니다.
-// 예: scheduledTask = cron.schedule('*/10 * * * * *', sendAutomaticPost);
+// 시작 작업 활성화
+startTask.start();

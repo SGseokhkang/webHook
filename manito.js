69a50b1a415f24d1b8c2b87885fd10666233667c
“seokhkang” <seokhkang@smilegate.com>
2023-12-11 14:08:09 +0900

    12/11

--- manito.js
+++ manito.js
@@ -3,16 +3,18 @@ const cron = require('node-cron');
 
 let count = 0; // 호출 횟수 추적
 let scheduledTask = cron.schedule('*/10 * * * * *', sendAutomaticPost); // cron 작업 초기 설정
+let maxAttempts = 6; // 최대 시도 횟수
 
 function sendAutomaticPost() {
     count++;
     console.log(`Function called ${count} times`);
 
-    if (count >= 6) {
-        console.log('Stopping cron job after 5 calls');
-        scheduledTask.destroy(); // cron 작업 완전히 제거
-        scheduledTask = null; // scheduledTask 변수 초기화
-        return; // 함수 실행 중단
+    // 최대 시도 횟수 도달 시 cron 작업 중지
+    if (count >= maxAttempts) {
+        console.log(`Stopping cron job after ${maxAttempts} attempts`);
+        scheduledTask.destroy();
+        scheduledTask = null;
+        return;
     }
 
     fetch('https://port-0-webhook-6w1j2allyzyvdu.sel5.cloudtype.app/manitoBot', {
@@ -36,8 +38,12 @@ function sendAutomaticPost() {
     })
     .catch(error => {
         console.error('Error:', error);
+        if (scheduledTask) {
+            count--; // 오류 발생 시 count 감소
+            console.log('Attempting to resend...');
+        }
     });
 }
 
-// 이 코드를 사용하여 필요한 경우 cron 작업을 다시 시작할 수 있습니다.
+// 필요한 경우 cron 작업을 다시 시작할 수 있습니다.
 // 예: scheduledTask = cron.schedule('*/10 * * * * *', sendAutomaticPost);
